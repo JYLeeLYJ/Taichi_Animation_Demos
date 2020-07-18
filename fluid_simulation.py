2d382e62ec3d0bea3ff247f97fe71b7a48f0f597
JYLeeLYJ <JYLeeLYJ@qq.com>
2020-07-19 05:20:54 +0800

    add MGPCG for smoke2d

--- fluid_simulation.py
+++ fluid_simulation.py
@@ -4,10 +4,11 @@ import numpy as np
 ti.init(arch=ti.gpu)
 
 #resolution
-res = 600
+resolution = 600
 
 color = (np.random.rand(3) * 0.7) + 0.3
 
+@ti.data_oriented
 class Pair :
     def __init__ (self , curr, next):
         self.curr = curr
@@ -16,10 +17,188 @@ class Pair :
     def swap(self):
         self.curr , self.next = self.next , self.curr
 
+# multigrid preconditioned conjugate gradient method
+@ti.data_oriented
+class MGPCG_Solver:
+
+    def __init__(self , n , dim = 2 , real = ti.f32 ):
+
+        # grid parameters
+        self.use_multigrid = True
+
+        self.N = n
+
+        self.n_mg_levels = 4
+        self.pre_and_post_smoothing = 2
+        self.bottom_smoothing = 50
+        self.dim = dim
+
+        self.N_ext = self.N // 2  # number of ext cells set so that that total grid size is still power of 2
+        self.N_tot = 2 * self.N
+
+        # setup sparse simulation data arrays
+        self.r = [ti.var(dt=real) for _ in range(self.n_mg_levels)]  # residual
+        self.z = [ti.var(dt=real) for _ in range(self.n_mg_levels)]  # M^-1 self.r
+        self.x = ti.var(dt=real)  # solution
+        self.p = ti.var(dt=real)  # conjugate gradient
+        self.Ap = ti.var(dt=real)  # matrix-vector product
+        self.alpha = ti.var(dt=real)  # step size
+        self.beta = ti.var(dt=real)  # step size
+        self.sum = ti.var(dt=real)  # storage for reductions
+
+        indices = ti.ijk if self.dim == 3 else ti.ij
+        self.grid = ti.root.pointer(indices, [self.N_tot // 4]).dense(
+            indices, 4).place(self.x, self.p, self.Ap)
+
+        for l in range(self.n_mg_levels):
+            self.grid = ti.root.pointer(indices,
+                                        [self.N_tot // (4 * 2**l)]).dense(
+                                            indices,
+                                            4).place(self.r[l], self.z[l])
+
+        ti.root.place(self.alpha, self.beta, self.sum)
+
+    @ti.func
+    def init(self , r0):
+        for I in ti.grouped(
+                ti.ndrange(*((self.N_ext, self.N_tot - self.N_ext), ) * self.dim)):
+            self.z[0][I] = 0.0
+            self.Ap[I] = 0.0
+            self.p[I] = 0.0
+            self.x[I] = 0.0
+        
+        self.r[0].copy_from(r0)
+
+    @ti.func
+    def neighbor_sum(self, x, I):
+        ret = 0.0
+        for i in ti.static(range(self.dim)):
+            offset = ti.Vector.unit(self.dim, i)
+            ret += x[I + offset] + x[I - offset]
+        return ret
+
+    # TODO need seperated
+    @ti.func
+    def compute_Ap(self):
+        for I in ti.grouped(self.Ap):
+            self.Ap[I] = (2 * self.dim) * self.p[I] - self.neighbor_sum(self.p, I)
+
+    @ti.func
+    def reduce(self, p , q ):
+        self.sum[None] = 0
+        for I in ti.grouped(p):
+            self.sum[None] += p[I] * q[I]
+
+    @ti.func
+    def update_x(self):
+        for I in ti.grouped(self.p):
+            self.x[I] += self.alpha[None] * self.p[I]
+
+    @ti.func
+    def update_r(self):
+        for I in ti.grouped(self.p):
+            self.r[0][I] -= self.alpha[None] * self.Ap[I]
+
+    @ti.func
+    def update_p(self):
+        for I in ti.grouped(self.p):
+            self.p[I] = self.z[0][I] + self.beta[None] * self.p[I]
+
+    @ti.func
+    def restrict(self, l):
+        for I in ti.grouped(self.r[l]):
+            res = self.r[l][I] - (2.0 * self.dim * self.z[l][I] -
+                                  self.neighbor_sum(self.z[l], I))
+            self.r[l + 1][I // 2] += res * 0.5
+
+    @ti.func
+    def prolongate(self, l):
+        for I in ti.grouped(self.z[l]):
+            self.z[l][I] = self.z[l + 1][I // 2]
+
+    @ti.func
+    def smooth(self, l, phase):
+        # phase = red/black Gauss-Seidel phase
+        for I in ti.grouped(self.r[l]):
+            if (I.sum()) & 1 == phase:
+                self.z[l][I] = (self.r[l][I] + self.neighbor_sum(
+                    self.z[l], I)) / (2.0 * self.dim)
+
+    @ti.func
+    def apply_preconditioner(self):
+        self.z[0].fill(0)
+        for l in range(self.n_mg_levels - 1):
+            for _ in range(self.pre_and_post_smoothing << l):
+                self.smooth(l, 0)
+                self.smooth(l, 1)
+            self.z[l + 1].fill(0)
+            self.r[l + 1].fill(0)
+            self.restrict(l)
+
+        for _ in range(self.bottom_smoothing):
+            self.smooth(self.n_mg_levels - 1, 0)
+            self.smooth(self.n_mg_levels - 1, 1)
+
+        for l in reversed(range(self.n_mg_levels - 1)):
+            self.prolongate(l)
+            for _ in range(self.pre_and_post_smoothing << l):
+                self.smooth(l, 1)
+                self.smooth(l, 0)
+
+    @ti.func
+    def solve(self , r0):
+        self.init(r0)
+        self.reduce(self.r[0], self.r[0])
+        initial_rTr = self.sum[None]
+
+        # self.r = b - Ax = b    since self.x = 0
+        # self.p = self.r = self.r + 0 self.p
+        self.apply_preconditioner()
+
+        self.update_p()
+
+        self.reduce(self.z[0], self.r[0])
+        old_zTr = self.sum[None]
+
+        # CG
+        for _ in range(400):
+            # self.alpha = rTr / pTAp
+            self.compute_Ap()
+            self.reduce(self.p, self.Ap)
+            pAp = self.sum[None]
+            self.alpha[None] = old_zTr / pAp
+
+            # self.x = self.x + self.alpha self.p
+            self.update_x()
+
+            # self.r = self.r - self.alpha self.Ap
+            self.update_r()
+
+            # check for convergence
+            self.reduce(self.r[0], self.r[0])
+            rTr = self.sum[None]
+            if rTr < initial_rTr * 1.0e-12:
+                break
+
+            # self.z = M^-1 self.r
+            self.z[0].copy_from(self.r[0])
+
+            # self.beta = new_rTr / old_rTr
+            self.reduce(self.z[0], self.r[0])
+            new_zTr = self.sum[None]
+            self.beta[None] = new_zTr / old_zTr
+
+            # self.p = self.z + self.beta self.p
+            self.update_p()
+            old_zTr = new_zTr
+
 @ti.data_oriented
 class Smoke_Solver2D:
 
-    def __init__(self):
+    def __init__(self , res  ,use_mgpcg = False , use_dye = True):
+
+        self.res = res
+
         ### physical grid
         # pressure field
         self._pressure_curr = ti.var(dt = ti.f32 , shape=(res , res))
@@ -53,21 +232,25 @@ class Smoke_Solver2D:
         # force
         self.f_strength = 10000.0
 
-        
         ### solver param
         # RK-order
         self.RK = 3
         # use_BFECC
         # self.use_BFECC = False
         # linear solver scheme
-        self.use_MGPCG = False # false then use jacobi
+        self.use_MGPCG = use_mgpcg # false then use jacobi
         # rendering option
-        self.use_dye = True # false then render vel_div field
+        self.use_dye = use_dye # false then render vel_div field
 
         ### swap pair
         self.velocity = Pair(self._velocity_curr , self._velocity_next)
         self.pressure = Pair(self._pressure_curr , self._pressure_next)
         self.dyeing   = Pair(self._dye_curr , self._dye_next)
+
+        ### MGPCG usage
+        self.r0 = ti.var(dt=ti.f32 , shape=(res , res))
+        if ti.static(self.use_MGPCG) :
+            self.mgpcg = MGPCG_Solver(n = res, dim = 2)
 
     ### ================ Advection ===========================
 
@@ -77,8 +260,8 @@ class Smoke_Solver2D:
 
     @ti.func
     def sample(self , field , u , v):
-        i = max(0, min(res - 1, int(u)))
-        j = max(0, min(res - 1, int(v)))
+        i = max(0, min(self.res - 1, int(u)))
+        j = max(0, min(self.res - 1, int(v)))
         return field[i, j]
 
     @ti.func
@@ -112,6 +295,7 @@ class Smoke_Solver2D:
         return p
 
     @ti.kernel
+    # @ti.func
     def advection(self , vf: ti.template() , field : ti.template() , next_field : ti.template()):
         self.semi_lagrange(vf, field , next_field , self.dt)
 
@@ -128,9 +312,10 @@ class Smoke_Solver2D:
     ### ================ External Force Effect ===========================
 
     @ti.kernel
+    # @ti.func
     def external_force(self , vf : ti.template()):
         f_strenght_dt = ti.static(self.f_strength * self.dt)
-        force_r = ti.static(res / 3.0)
+        force_r = ti.static(self.res / 3.0)
         inv_force_r = ti.static (1.0 / force_r)
         sx , sy = ti.static( self.source_x , self.source_y )
 
@@ -145,7 +330,7 @@ class Smoke_Solver2D:
 
     @ti.func
     def divergence_vel(self , field):
-        inv_dx = ti.static(1 / self.dx)
+        half_inv_dx = ti.static(0.5 / self.dx)
         for i , j in field:
             vl = self.sample(field, i - 1, j)[0]
             vr = self.sample(field, i + 1, j)[0]
@@ -155,28 +340,38 @@ class Smoke_Solver2D:
             # edge check
             if i == 0:
                 vl = -vc[0]
-            elif i == res - 1:
+            elif i == self.res - 1:
                 vr = -vc[0]
             if j == 0:
                 vb = -vc[1]
-            elif j == res - 1:
+            elif j == self.res - 1:
                 vt = -vc[1]
             # div_v
-            self.velocity_div[i, j] = (vr - vl + vt - vb) * 0.5 * inv_dx
+            div = (vr - vl + vt - vb) * half_inv_dx
+            self.velocity_div[i, j] = div
+            self.r0[i,j] = -1.0 * div
 
     @ti.kernel
+    # @ti.func
     def projection(self , v_cur : ti.template(), p : ti.template() ):
         self.divergence_vel(v_cur)
         if ti.static(self.use_MGPCG):
             self.MGPCG()
         else :
-            # jacobi iteration
+            # self.jacobi(p)
             for _ in ti.static(range(30)):
-                self.jacobi(p.curr , p.next)
+                self.jacobi_step(p.curr , p.next)
                 p.swap()
 
     @ti.func
-    def jacobi(self , p_cur , p_nxt):
+    def jacobi(self , p) :
+        # jacobi iteration
+        for _ in ti.static(range(30)):
+            self.jacobi_step(p.curr , p.next)
+            p.swap()
+
+    @ti.func
+    def jacobi_step(self , p_cur , p_nxt):
         dx_sqr = self.dx * self.dx
         for i , j in p_cur :
             pl = self.sample(p_cur , i - 1 , j)
@@ -193,6 +388,7 @@ class Smoke_Solver2D:
     ### ================ Rendering ===========================
 
     @ti.kernel
+    # @ti.func
     def update_v(self , vf : ti.template() , pf : ti.template()):
         half_inv_dx = ti.static( 0.5 / self.dx )
         for i,j in vf :
@@ -203,8 +399,9 @@ class Smoke_Solver2D:
             vf[i, j] = self.sample(vf, i, j) - half_inv_dx * ti.Vector([pr - pl, pt - pb])
 
     @ti.kernel
+    # @ti.func
     def update_dye(self , dyef : ti.template()):
-        inv_dye_denom = ti.static(4.0 / (res / 15.0)**2)
+        inv_dye_denom = ti.static(4.0 / (self.res / 15.0)**2)
         sx , sy = ti.static(self.source_x , self.source_y)
         for i , j in dyef :
             dx , dy = i + 0.5 - sx , j + 0.5 - sy
@@ -235,10 +432,11 @@ class Smoke_Solver2D:
         else :
             self.render_vel_div()
 
+    # @ti.kernel
     # For each solving step
     def step(self):
         # caculate advection for velocity field and pressure field
-        if self.use_dye :
+        if ti.static(self.use_dye) :
             self.advection(self.velocity.curr , self.velocity.curr , self.velocity.next)
             self.advection(self.velocity.curr , self.dyeing.curr , self.dyeing.next)
         else :
@@ -254,7 +452,7 @@ class Smoke_Solver2D:
         # update velocity field
         self.update_v(self.velocity.curr , self.pressure.curr)
         # update dyeing field
-        if self.use_dye:
+        if ti.static(self.use_dye):
             self.update_dye(self.dyeing.curr)
 
     def reset(self):
@@ -264,7 +462,7 @@ class Smoke_Solver2D:
         self.color.fill([0,0,0])
 
 def main(gui):
-    smk = Smoke_Solver2D()
+    smk = Smoke_Solver2D(res = resolution)
     smk.reset()
     while gui.running :
         # gui.get_event(ti.GUI.PRESS)
@@ -277,5 +475,5 @@ def main(gui):
         gui.show()
             
 if __name__ == "__main__":
-    gui = ti.GUI("Smoke Simulation" , res= res)
+    gui = ti.GUI("Smoke Simulation" , res= resolution)
     main(gui)
